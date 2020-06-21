cdab98c1e8dae300861b7cc734cae870aa682c73
Stacy Cashmore <33732891+StacyCash@users.noreply.github.com>
2020-06-21 16:22:35 +0200

    Update QueueAccess.cs

--- back-end/SignupApi/QueueHelper/QueueAccess.cs
+++ back-end/SignupApi/QueueHelper/QueueAccess.cs
@@ -9,7 +9,7 @@ namespace SignupApi.QueueHelper
 {
   public class QueueAccess
   {
-    private const string _connectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;";
+    private const string _connectionString = "<Your Azure Storage Account Connection String Here>";
     private readonly string _queueName;
 
     public QueueAccess(string queueName)
